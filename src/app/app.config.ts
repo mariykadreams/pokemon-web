1d1bcfd450765523d8a121e8b72f1a800e0e9936
Mariia Tokhtamysh <mariyka.tokhtamysh@gmail.com>
2025-12-13 20:05:43 +0100

    add firebase connection

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,13 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(), provideFirebaseApp(() => initializeApp({"projectId":"pokemon-web-e0a13","appId":"1:833729070492:web:9ac8d4e60d155b2dbcb459","storageBucket":"pokemon-web-e0a13.firebasestorage.app","apiKey":"AIzaSyBveU5fqem6Uym8TC2dIwRY9wlv66rfXEc","authDomain":"pokemon-web-e0a13.firebaseapp.com","messagingSenderId":"833729070492","measurementId":"G-JMRGWKD40S"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())
   ]
 };
